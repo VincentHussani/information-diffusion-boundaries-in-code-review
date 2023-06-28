ebf5274b7cb6669166bd9c96665089978ed4ff21
Vincent Honar <82162851+VincentHussani@users.noreply.github.com>
2023-06-28 14:28:06 +0200

    Added actual tests

--- test/test_plot.py
+++ test/test_plot.py
@@ -1,26 +1,31 @@
 import unittest
+import pandas as pd
+from plot import compute
 
-from simulation.model import CommunicationNetwork
+class TestCompute(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_df =  pd.read_pickle('../data/minimal_paths/spotify.pickle.bz2')
 
-# o
-class ModelTest(unittest.TestCase):
+    def test_reproducability(self):
+        df1 = self.test_df.copy(deep=True)
+        df2 = self.test_df.copy(deep=True)
 
-    cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
+        result1 = compute(df1)
+        result2 = compute(df2)
 
-    def test_vertices(self):
-        self.assertEqual(len(ModelTest.cn.vertices()), 4)
-        self.assertEqual(ModelTest.cn.vertices('h1'), {'v1', 'v2'})
+        pd.testing.assert_frame_equal(result1, result2)
 
-    def test_hyperedges(self):
-        self.assertEqual(len(ModelTest.cn.hyperedges()), 3)
-        self.assertEqual(ModelTest.cn.hyperedges('v1'), {'h1'})
+    def test_invalid_input(self):
+        pass
 
+    def test_cumsum(self):
+        pass
 
-class ModelDataTest(unittest.TestCase):
-    def test_model_with_data(self):
-        communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
-        self.assertEqual(len(communciation_network.participants()), 37103)
-        self.assertEqual(len(communciation_network.channels()), 309740)
+    def test_missing(self):
+        pass
 
-        self.assertEqual(len(communciation_network.vertices()), 37103)
-        self.assertEqual(len(communciation_network.hyperedges()), 309740)
+    def test_negative(self):
+        pass
+
+    def test_same(self):
+        pass
