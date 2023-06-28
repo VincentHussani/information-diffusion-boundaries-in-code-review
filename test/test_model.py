3d6ab8bfab4480c1428f9775acb6074314f8598a
Vincent Honar <82162851+VincentHussani@users.noreply.github.com>
2023-06-28 19:19:46 +0200

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -1,26 +1,67 @@
 import unittest
+from collections import defaultdict
+import os
+import sys
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from simulation.model import TimeVaryingHypergraph, CommunicationNetwork
 
-from simulation.model import CommunicationNetwork
-
-#mmamamama
-class ModelTest(unittest.TestCase):
-
-    cn = CommunicationNetwork({'h1': ['v1', 'v2'], 'h2': ['v2', 'v3'], 'h3': ['v3', 'v4']}, {'h1': 1, 'h2': 2, 'h3': 3})
-
+class TestTimeVaryHypergraph(unittest.TestCase):
+    """Tests for `model.py` module"""
+    def test_class_creation(self):
+        """Test that the class(TimeVaryingHypergraph) can be instantiated"""
+        hedges = {'hedge1': ['v1', 'v2'], 'hedge2': ['v2', 'v3']}
+        timings = {'v1': [1, 2, 3], 'v2': [2, 3, 4], 'v3': [3, 4, 5]}
+        cls = TimeVaryingHypergraph(hedges=hedges, timings=timings)
+        self.assertEqual(cls._hedges, hedges)
+        self.assertEqual(cls._timings, timings)
+        self.assertIsInstance(cls, TimeVaryingHypergraph)
+    def test_timings(self):
+        """Test that the timings method works and returns the correct values"""
+        timings = {'v1': [1, 2, 3], 'v2': [2, 3, 4], 'v3': [3, 4, 5]}
+        tvh = TimeVaryingHypergraph(hedges={}, timings=timings)
+        timings_returned = tvh.timings()
+        self.assertIsInstance(timings_returned, dict)
     def test_vertices(self):
-        self.assertEqual(len(ModelTest.cn.vertices()), 4)
-        self.assertEqual(ModelTest.cn.vertices('h1'), {'v1', 'v2'})
+        """ Test that the vertices method works and returns the correct values without hedge"""
+        hedges = {'hedge1': ['v1', 'v2'], 'hedge2': ['v2', 'v3']}
+        tvh = TimeVaryingHypergraph(hedges=hedges, timings={})
+        vertices_returned = tvh.vertices()
+        self.assertIsInstance(vertices_returned, set)
+        self.assertEqual(vertices_returned, {'v1', 'v2', 'v3'})
+
+    def test_vertices_hedge(self):
+        """Test that the vertices method works and returns the correct values with hedge"""
+        hedges = {'hedge1': ['v1', 'v2'], 'hedge2': ['v2', 'v3']}
+        tvh = TimeVaryingHypergraph(hedges=hedges, timings={})
+        vertices_returned = tvh.vertices(hedge='hedge1')
+        self.assertIsInstance(vertices_returned, set)
+        self.assertEqual(vertices_returned, {'v1', 'v2'})
 
     def test_hyperedges(self):
-        self.assertEqual(len(ModelTest.cn.hyperedges()), 3)
-        self.assertEqual(ModelTest.cn.hyperedges('v1'), {'h1'})
+        """Test that the hyperedges method works and returns the correct values without vertex"""
+        hedges = {'hedge1': ['v1', 'v2'], 'hedge2': ['v2', 'v3']}
+        tvh = TimeVaryingHypergraph(hedges=hedges, timings={})
+        hyperedges_returned = tvh.hyperedges()
+        self.assertIsInstance(hyperedges_returned, set)
+        self.assertEqual(hyperedges_returned, {'hedge1', 'hedge2'})
+
+    def test_hyperedges_hedge(self):
+        """Test that the hyperedges method works and returns the correct values"""
+        hedges = {'hedge1': ['v1', 'v2'], 'hedge2': ['v2', 'v3']}
+        tvh = TimeVaryingHypergraph(hedges=hedges, timings={})
+        hyperedges_returned = tvh.hyperedges(vertex='v1')
+        self.assertIsInstance(hyperedges_returned, set)
+        self.assertEqual(hyperedges_returned, {'hedge1'})
 
 
-class ModelDataTest(unittest.TestCase):
-    def test_model_with_data(self):
-        communciation_network = CommunicationNetwork.from_json('./data/networks/microsoft.json.bz2')
-        self.assertEqual(len(communciation_network.participants()), 37103)
-        self.assertEqual(len(communciation_network.channels()), 309740)
 
-        self.assertEqual(len(communciation_network.vertices()), 37103)
-        self.assertEqual(len(communciation_network.hyperedges()), 309740)
+class TestCommunicationNetwork(unittest.TestCase):
+    def test_class_creation(self):
+        """Test that the class(CommunicationNetwork) can be instantiated"""
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
