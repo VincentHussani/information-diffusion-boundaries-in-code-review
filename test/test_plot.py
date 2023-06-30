06b93c32f02fe31f059d1a849e9f125605e2d14e
Vincent Honar <82162851+VincentHussani@users.noreply.github.com>
2023-07-01 00:35:40 +0200

    CI compatible

--- test/test_plot.py
+++ test/test_plot.py
@@ -5,10 +5,9 @@ import nbformat
 class TestCompute(unittest.TestCase):
     def setUp(self) -> None:
         self.test_df =  pd.read_pickle('data/minimal_paths/spotify.pickle.bz2')
-
+        self.expected_shape = (29,1730)
         with open('notebooks/plot.ipynb',encoding="utf-8") as f:
             self.notebook = nbformat.read(f,as_version=4)
-
         assert 'def compute' in self.notebook.cells[1].source
 
         import_code = self.notebook.cells[0].source
@@ -18,58 +17,40 @@ class TestCompute(unittest.TestCase):
 
         exec_globals = {}
         exec(import_code+"\n"+compute_code, exec_globals)
-
         self.compute = exec_globals['compute']
 
     def test_reproducability(self):
-
-
         result1 = self.compute(self.test_df)
         result2 = self.compute(self.test_df)
-
+        print(result1)
         pd.testing.assert_frame_equal(result1, result2)
 
-    def test_invalid_input(self):
-        pass
-
     def test_cumsum(self):
-        pass
+        cumsum_df = pd.DataFrame({"a":[1,2,3,4,5],"b":[1,2,3,4,5]})
+        calculated_cumsum = pd.DataFrame({"a":[1,3,6,10,15],"b":[1,3,6,10,15]})
+        pd.testing.assert_frame_equal(cumsum_df.cumsum(),calculated_cumsum)
 
     def test_negative(self):
         df = self.test_df.copy(deep=True)
-
+        df["shortest"] = - df["shortest"]
         df["fastest"] = - df["fastest"]
-
         result = self.compute(df)
-
-        num_columns = result.shape[1]
-        expected_columns = 3
-
-        self.assertTrue(isinstance(result, pd.DataFrame))
-        self.assertEqual(num_columns, expected_columns)
+        self.assertEqual(result.shape, self.expected_shape)
 
 
     def test_same(self):
         df = self.test_df.copy(deep=True)
-
-        df["fastest"] = df["fastest"][1]
-
+        df["fastest"] = df["fastest"][0]
+        df["shortest"] = 90000000
         result = self.compute(df)
-
-        num_columns = result.shape[1]
-        expected_columns = 3
-
-        self.assertTrue(isinstance(result, pd.DataFrame))
-        self.assertEqual(num_columns, expected_columns)
+        self.assertEqual(result.shape, self.expected_shape)
 
     def test_missing(self):
         df = self.test_df.copy(deep=True)
 
-        df["fastest"] = None
+        df["shortest"] = None
+        df["target"] = None
+        df["source"] = None
         result = self.compute(df)
-
-        num_columns = result.shape[1]
-        expected_columns = 3
-
         self.assertTrue(isinstance(result, pd.DataFrame))
-        self.assertEqual(num_columns, expected_columns)
+        self.assertEqual(result.shape, self.expected_shape)
