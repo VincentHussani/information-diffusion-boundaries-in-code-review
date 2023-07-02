936b618706447a451778b91c439f7b28b74b6810
Vincent Honar <82162851+VincentHussani@users.noreply.github.com>
2023-07-02 18:03:20 +0200

    Add files via upload

--- .github/get_tests.py
+++ .github/get_tests.py
@@ -2,17 +2,21 @@
 import subprocess
 import os
 import yaml
-
+found_notebook = False
 
 def get_changed_files():  # Uses githubs inbuilt functions to get changed files
     changed_files_json = subprocess.run(["git", "diff", "--name-only","HEAD^"], stdout=subprocess.PIPE, check=True)
     changed_files = changed_files_json.stdout.decode("utf-8").split("\n")
     return changed_files
 
+def notebook_checker(file):
+    if found_notebook is False and file["extension"] == ".ipynb":
+                    subprocess.run(["pip","install","nbformat","matplotlib","numpy"],stdout=subprocess.PIPE,check=True)
+                    found_notebook = True
+
 
 def get_required_tests(changed_files, testable_file): # compares the files to the ones in the dependencies yaml file to see which needs to be tested
     tests_to_run = []
-    has_notebook = False
     for file in changed_files:
         base_name = os.path.basename(file)
         base_name = os.path.splitext(base_name)[0]
@@ -24,17 +28,11 @@ def get_required_tests(changed_files, testable_file): # compares the files to th
 
             if i["name"] == base_name:
                 tests_to_run.append(i["name"]) #Found a file which is supposed to be tested
-
-                if has_notebook is False and i["extension"] == ".ipynb":
-                    subprocess.run(["pip","install","nbformat","matplotlib","numpy"],stdout=subprocess.PIPE,check=True)
-                    has_notebook = True
-
+                notebook_checker(i)
             elif set(i["dependencies"]).intersection(tests_to_run) or base_name in i["dependencies"]:
                 tests_to_run.append(i["name"]) #Found a file which depends on a tested file
+                notebook_checker(i)
 
-                if has_notebook is False and i["extension"] == ".ipynb":
-                    subprocess.run(["pip","install","nbformat","matplotlib","numpy"],stdout=subprocess.PIPE,check=True)
-                    has_notebook = True
 
     return set(tests_to_run)
 
@@ -45,8 +43,6 @@ if __name__ == "__main__":
         testable_files = yaml.safe_load(file)
     changed_files = get_changed_files()
     tests_to_run = get_required_tests(changed_files, testable_files)
-    tests_to_run = get_required_tests(changed_files, testable_files)
-
     existing_tests = []
     for test_file in tests_to_run:
         test_path = f"test/test_{test_file}.py"
